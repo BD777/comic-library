72f8d23182afd269b4de27d83dd41bbd4e55e05f
windeng <mis_tletoe@foxmail.com>
2021-05-15 22:02:40 +0800

    new project

---
+++ src/renderer/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import { Button, Menu, Icon } from 'ant-design-vue'
+
+[Button, Menu, Icon].forEach(e => {
+  Vue.use(e)
+})
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
